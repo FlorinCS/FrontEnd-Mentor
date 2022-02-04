46ae0fe171a39271e7035d43f4c6e3dde1e78b9f
FlorinCS <81389602+FlorinCS@users.noreply.github.com>
2022-02-04 21:21:51 +0100

    Add files via upload

---
+++ Coming Soon Apparel/Test.js
@@ -0,0 +1,11 @@
+/*const mq = window.matchMedia( "(max-width: 993px)" );
+doi=document.getElementById("doi");
+if (mq.matches) {
+       doi.style.visibility="hidden"
+} else{
+
+}*/
+doi=document.getElementById("doi")
+function da(){
+    doi.style.visibility="hidden";
+}
